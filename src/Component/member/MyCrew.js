58d36f71951071bac2368370b0bd2517f7cad9d9
justlikeryu <justlikeryuu@gmail.com>
2023-12-19 18:44:43 +0900

    Design: MyCrew css 추가

--- src/Component/member/MyCrew.js
+++ src/Component/member/MyCrew.js
@@ -1,7 +1,20 @@
-export default function MyCrew(){
-    return(
+import {Image} from "react-bootstrap";
+import "./MyCrew.css"
+
+export default function MyCrew() {
+    return (
         <>
-        <h1>나의 모임</h1>
+            <h1>나의 모임</h1>
+            <div id="list">
+                <div id="photo">사진</div>
+                <div id="moim-info">
+                    <div id="name">이름</div>
+                    <div>
+                        <div>카테고리</div>
+                        <div>인원</div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
