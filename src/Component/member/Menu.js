66700631d02f21a8b7b4ac65631177c821e911fd
justlikeryu <justlikeryuu@gmail.com>
2023-12-19 17:35:09 +0900

    Feat: 회원 메인페이지용 사이드바 추가

--- src/Component/member/Menu.js
+++ src/Component/member/Menu.js
@@ -1,17 +1,39 @@
-import {Link} from "react-router-dom";
+import "./Menu.css";
+import {MenuData} from "./MenuData";
 
-export default function Menu(){
-    return(
-        <div className="menu">
-            <ul>
-                <li><Link to="/member/info">회원정보</Link></li>
-                <li><Link to="/member/message">메세지함</Link></li>
-                <li><Link to="/member/crew">나의모임</Link></li>
-                <li><Link to="/member/review">리뷰관리</Link></li>
-                <li><Link to="/member/delete">회원탈퇴</Link></li>
-                {/*<li><Link to="/member/likecrew">찜한크루</Link></li>*/}
-                {/*<li><Link to="/member/partincrew">참가크루</Link></li>*/}
+export default function Menu() {
+    return (
+        <div className="sidebar">
+
+            {/*<ul id="menu">*/}
+            {/*<li><button><Link to="/member/mypage">회원정보</Link></button></li>*/}
+            {/*<li><button><Link to="/member/message">메세지함</Link></button></li>*/}
+            {/*<li><button><Link to="/member/mycrew">나의모임</Link></button></li>*/}
+            {/*<li><button><Link to="/member/mycrew/partin">참가모임</Link></button></li>*/}
+            {/*<li><button><Link to="/member/mycrew/like">관심모임</Link></button></li>*/}
+            {/*<li><button><Link to="/member/review">리뷰관리</Link></button></li>*/}
+
+            {/*<li><Link to="/member/mypage"><button>회원정보</button></Link></li>*/}
+            {/*<li><Link to="/member/message"><button>메세지함</button></Link></li>*/}
+            {/*<li><Link to="/member/mycrew"><button>나의모임</button></Link></li>*/}
+            {/*<li><Link to="/member/mycrew/partin"><button>참가모임</button></Link></li>*/}
+            {/*<li><Link to="/member/mycrew/like"><button>관심모임</button></Link></li>*/}
+            {/*<li><Link to="/member/review"><button>리뷰관리</button></Link></li>*/}
+            {/*</ul>*/}
+            <ul className="sidebarList">
+                {MenuData.map((val, key) => {
+                    return (
+                        <li key={key} className="row" id={window.location.pathname === val.link ? "active" : ""}
+                            onClick={() => {
+                                window.location.pathname = val.link
+                            }}>
+                            <div id="icon">{val.icon}</div>
+                            <div id="title">{val.title}</div>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
 }
+
---
+++ src/Component/member/MenuData.js
@@ -0,0 +1,32 @@
+export const MenuData = [
+    {
+        title: "마이페이지",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/mypage"
+    },
+    {
+        title: "메세지함",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/message"
+    },
+    {
+        title: "나의모임",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/mycrew"
+    },
+    {
+        title: "관심모임",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/mycrew/like"
+    },
+    {
+        title: "참가모임",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/mycrew/partin"
+    },
+    {
+        title: "리뷰관리",
+        icon: <img src="/public/img/home-logo.png"/>,
+        link: "/member/review"
+    }
+]
