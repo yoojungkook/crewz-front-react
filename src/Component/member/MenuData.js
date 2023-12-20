98bfac59a38861f27823d109e7d8bbe5b6acd6ef
justlikeryu <justlikeryuu@gmail.com>
2023-12-20 18:45:56 +0900

    Feat: 메뉴별 아이콘 적용

--- src/Component/member/MenuData.js
+++ src/Component/member/MenuData.js
@@ -1,37 +1,39 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
 export const MenuData = [
     {
         title: "홈",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-house-door"></i>,
+    // <img src="/assets/icons/bootstrap.svg" alt="Bootstrap" width="32" height="32">
         link: "/"
     },
     {
         title: "마이페이지",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-person"></i>,
         link: "/member/mypage"
     },
     {
         title: "메세지함",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-envelope"></i>,
         link: "/member/message"
     },
     {
         title: "나의모임",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-people"></i>,
         link: "/member/mycrew"
     },
     {
         title: "관심모임",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-heart"></i>,
         link: "/member/mycrew/like"
     },
     {
         title: "참가모임",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-geo"></i>,
         link: "/member/mycrew/partin"
     },
     {
         title: "리뷰관리",
-        icon: <img src="/public/img/home-logo.png"/>,
+        icon: <i className="bi bi-card-text"></i>,
         link: "/member/review"
     }
 ]
