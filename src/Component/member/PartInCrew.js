c0582c34993668be8787bdff48779801e64aa097
justlikeryu <justlikeryuu@gmail.com>
2023-12-20 18:44:24 +0900

    Feat: 참가모임 페이지에 모달창 추가

--- src/Component/member/PartInCrew.js
+++ src/Component/member/PartInCrew.js
@@ -1,7 +1,12 @@
-import {Col, Image, Row} from "react-bootstrap";
+import {Button, Col, Form, Image, Row} from "react-bootstrap";
 import "./MyCrew.css"
+import Modal from "react-bootstrap/Modal";
+import {useState} from "react";
 
 export default function PartInCrew(){
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return(
         <>
             <br/>
@@ -16,13 +21,48 @@ export default function PartInCrew(){
                         <div id="crew-info">
                             <div id="crew-name">참가한 모임</div>
                             <div id="crew-etc">
-                                <span>지역</span> | <span>카테고리</span> | <span>인원</span>
+                                <span>지역</span> | <span>카테고리</span> | <span>인원</span><br/>
+                            </div>
+                            <span>참여일자</span>
+                            <div className="container-fluid d-flex justify-content-end">
+                                <Button variant="danger" onClick={handleShow}>리뷰 등록</Button>
                             </div>
                         </div>
                     </Col>
                 </Row>
-                <hr/>
             </div>
+            <hr/>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>리뷰 작성하기</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>참가한 소모임명</Form.Label>
+                            <Form.Control type="text"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>제목</Form.Label>
+                            <Form.Control type="text"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>내용</Form.Label>
+                            <Form.Control as="textarea" rows={2} style={{resize: "none"}}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>사진</Form.Label>
+                            <Form.Control type="file"/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button type="submit" variant="danger" onClick={handleClose}>
+                        작성
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
