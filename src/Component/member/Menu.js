0858c35eb32a914cde7aa382f81f066ae2d43162
justlikeryu <justlikeryuu@gmail.com>
2023-12-20 17:39:22 +0900

    Feat: 주석 삭제 및 아이콘 라이브러리 추가

--- src/Component/member/Menu.js
+++ src/Component/member/Menu.js
@@ -4,22 +4,6 @@ import {MenuData} from "./MenuData";
 export default function Menu() {
     return (
         <div className="sidebar">
-
-            {/*<ul id="menu">*/}
-            {/*<li><button><Link to="/member/mypage">회원정보</Link></button></li>*/}
-            {/*<li><button><Link to="/member/message">메세지함</Link></button></li>*/}
-            {/*<li><button><Link to="/member/mycrew">나의모임</Link></button></li>*/}
-            {/*<li><button><Link to="/member/mycrew/partin">참가모임</Link></button></li>*/}
-            {/*<li><button><Link to="/member/mycrew/like">관심모임</Link></button></li>*/}
-            {/*<li><button><Link to="/member/review">리뷰관리</Link></button></li>*/}
-
-            {/*<li><Link to="/member/mypage"><button>회원정보</button></Link></li>*/}
-            {/*<li><Link to="/member/message"><button>메세지함</button></Link></li>*/}
-            {/*<li><Link to="/member/mycrew"><button>나의모임</button></Link></li>*/}
-            {/*<li><Link to="/member/mycrew/partin"><button>참가모임</button></Link></li>*/}
-            {/*<li><Link to="/member/mycrew/like"><button>관심모임</button></Link></li>*/}
-            {/*<li><Link to="/member/review"><button>리뷰관리</button></Link></li>*/}
-            {/*</ul>*/}
             <ul className="sidebarList">
                 {MenuData.map((val, key) => {
                     return (
