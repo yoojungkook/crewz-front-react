ab644379cf0769db0a09d046ceedbdc7e40790ab
justlikeryu <justlikeryuu@gmail.com>
2023-12-25 21:56:14 +0900

    Design: 모달창 footer내 줄바꿈 적용

--- src/Component/guest/Login.js
+++ src/Component/guest/Login.js
@@ -1,14 +1,13 @@
-import { Button, Modal } from "react-bootstrap";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import {Button, Modal} from "react-bootstrap";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import Join from "./Join";
 import FindId from "./FindId";
 import FindPwd from "./FindPwd";
+import "./Login.css"
 
-;
-
-export default function Login({ show, handleClose }) {
+export default function Login({show, handleClose}) {
     const [showSignUpModal, setShowSignUpModal] = useState(false);
     const openSignUpModal = () => {
         setShowSignUpModal(true)
@@ -32,12 +31,12 @@ export default function Login({ show, handleClose }) {
     }
     const closeFindPwdModal = () => setShowFindPwdModal(false);
 
-    const [inputs, setInputs] = useState({ id: '', pwd: '' });
-    const { id, pwd } = inputs;
+    const [inputs, setInputs] = useState({id: '', pwd: ''});
+    const {id, pwd} = inputs;
     const navigate = useNavigate();
 
     const onChange = (e) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setInputs({
             ...inputs,
             [name]: value
@@ -45,7 +44,7 @@ export default function Login({ show, handleClose }) {
     }
 
     const signIn = () => {
-        axios.post('http://crewz.asuscomm.com/api/member/login', {}, { params: { id: id, pwd: pwd } })
+        axios.post('http://crewz.asuscomm.com/api/member/login', {}, {params: {id: id, pwd: pwd}})
             .then(function (res) {
                 if (res.status === 200) {
                     if (res.data.flag) {
@@ -72,26 +71,27 @@ export default function Login({ show, handleClose }) {
 
                 <Modal.Body>
                     <div className="form-el">
-                        <label htmlFor="id">아이디</label> <br />
-                        <input type="text" name="id" value={id} onChange={onChange} />
+                        <label htmlFor="id">아이디</label> <br/>
+                        <input type="text" name="id" value={id} onChange={onChange}/>
                     </div>
 
                     <div className="form-el">
-                        <label htmlFor="pwd">비밀번호</label> <br />
-                        <input type="password" id="pwd" name="pwd" value={pwd} onChange={onChange} />
+                        <label htmlFor="pwd">비밀번호</label> <br/>
+                        <input type="password" id="pwd" name="pwd" value={pwd} onChange={onChange}/>
                     </div>
                 </Modal.Body>
 
-                <Modal.Footer>
+                <Modal.Footer id="footer">
                     <Button variant="danger" onClick={signIn}>로그인</Button>
-                    <a href="#" onClick={openSignUpModal}>회원가입</a>
-                    <a href="#" onClick={openFindIdModal}>아이디찾기</a>
-                    <a href="#" onClick={openFindPwdModal}>비밀번호찾기</a>
+
+                    <div>
+                        <a href="#" onClick={openSignUpModal}>회원가입</a> | <a href="#" onClick={openFindIdModal}>아이디찾기</a> | <a href="#" onClick={openFindPwdModal}>비밀번호찾기</a>
+                    </div>
                 </Modal.Footer>
 
             </Modal>
 
-            <Join show={showSignUpModal} handleClose={closeSignUpModal} />
+            <Join show={showSignUpModal} handleClose={closeSignUpModal}/>
             <FindId show={showFindIdModal} handleClose={closeFindIdModal}/>
             <FindPwd show={showFindPwdModal} handleClose={closeFindPwdModal}/>
         </div>
