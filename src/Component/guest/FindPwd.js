641a3776d12f939bc9999d4cf4dfbe328e0ff48a
justlikeryu <justlikeryuu@gmail.com>
2023-12-26 12:28:30 +0900

    Feat: 비밀번호 찾을 수 있도록 수정

--- src/Component/guest/FindPwd.js
+++ src/Component/guest/FindPwd.js
@@ -22,8 +22,10 @@ export default function FindPwd({show, handleClose}) {
         axios.post('http://crewz.asuscomm.com/api/member/find/pwd', {}, {params: {id: id, tel: tel}})
             .then(function (res) {
                 if (res.status === 200) {
-                    if (res.data.result !== null) {
-                        alert('pwd' + res.data.result);
+                    if (res.data.result !== 'null') {
+                        alert('회원님의 비밀번호는 ' + res.data.result + '입니다.');
+                    }else{
+                        alert('비밀번호를 찾을 수 없습니다.');
                     }
                 } else {
                     alert('error:' + res.status);
